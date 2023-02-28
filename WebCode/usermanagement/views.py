382f686ff8eb4234e17c8ee605d15140d66d227d
Walysson Paiva <walyssonpaiva@outlook.com>
2023-02-28 20:06:39 -0300

    Add files via upload

--- WebCode/usermanagement/views.py
+++ WebCode/usermanagement/views.py
@@ -11,7 +11,7 @@ def login_view(request):
         password = request.POST['password']
 
         # Open a gRPC channel to the remote service
-        with grpc.insecure_channel('localhost:50051') as channel:
+        with grpc.insecure_channel('34.86.129.130:50051') as channel:
             # Create a gRPC stub for the IoTService service
             stub = iot_service_pb2_grpc.IoTServiceStub(channel)
 
@@ -34,7 +34,7 @@ def register_view(request):
         password = request.POST['password']
 
         # Open a gRPC channel to the remote service
-        with grpc.insecure_channel('localhost:50051') as channel:
+        with grpc.insecure_channel('34.86.129.130:50051') as channel:
             # Create a gRPC stub for the IoTService service
             stub = iot_service_pb2_grpc.IoTServiceStub(channel)
 
@@ -63,37 +63,38 @@ def light_sensor_view(request):
 
 def temperature(request):
     token = request.headers['Authorization']
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('34.86.129.130:50051')
     stub = iot_service_pb2_grpc.IoTServiceStub(channel)
     response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor', token=token))
     return JsonResponse({'temperature': response.temperature})
 
 def light_sensor(request):
     token = request.headers['Authorization']
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('34.86.129.130:50051')
     stub = iot_service_pb2_grpc.IoTServiceStub(channel)
     response = stub.SayLightLevel(iot_service_pb2.LightLevelRequest(sensorName='my_sensor', token=token))
     return JsonResponse({'light_sensor': response.lightLevel})
 
 def action(request):
     token = request.headers['Authorization']
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('34.86.129.130:50051')
     stub = iot_service_pb2_grpc.IoTServiceStub(channel)
     response = stub.Action(iot_service_pb2.ActionRequest(action='there_is_action?', token=token))
+    print("action",response)
     return JsonResponse({'action': response.status})
 
 def led(request):
     token = request.headers['Authorization']
     state = int(request.GET['state'])
     ledname = request.GET['ledname']
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('34.86.129.130:50051')
     stub = iot_service_pb2_grpc.IoTServiceStub(channel)
     response = stub.BlinkLed(iot_service_pb2.LedRequest(state=state, ledname=ledname, token=token))
-    return JsonResponse({'status': response.status})
+    return JsonResponse({'status': str(response.ledstate)})
 
 def led_status(request):
     token = request.headers['Authorization']
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('34.86.129.130:50051')
     stub = iot_service_pb2_grpc.IoTServiceStub(channel)
     response = stub.LightStatus(iot_service_pb2.LedStatusRequest(token=token))
     return JsonResponse({'green': response.status_green, 'red': response.status_red})
